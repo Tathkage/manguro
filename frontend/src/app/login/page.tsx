399444571f1cd5710458c4b09435aadd75c368e3
Tathluach Chol <tathluach.krc@gmail.com>
2024-09-10 17:31:39 -0400

    Added Mascots
    Added login and sign-up mascots.

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import Image from 'next/image';
 import React, { useState } from 'react';
 import styles from '../../styles/login.module.css';
+import femaleMascot from '../../../public/images/login-avatar.png'
 
 export default function Login() {
   const [showPassword, setShowPassword] = useState(false);
@@ -11,31 +13,45 @@ export default function Login() {
   };
 
   return (
-    <section className={styles['login-section']}>
-      <h1>Login</h1>
-      <form className={styles['login-form']}>
-        <div className={styles['input-element']}>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" />
+    <>
+      <section>
+        <div className={styles['mascot-container']}>
+          <Image
+            src={femaleMascot}
+            alt="Female Manguro Mascot"
+            width={500}
+            height={500}
+            className={styles['login-avatar']}
+          />
         </div>
-        <div className={styles['input-element']}>
-          <label htmlFor="password">Password</label>
-          <div className={styles['password-wrapper']}>
-            <input
-              type={showPassword ? 'text' : 'password'}
-              id="password"
-              name="password"
-            />
-            <span className={styles["toggle-password"]} onClick={togglePasswordVisibility}>
-              {showPassword ? 'HIDE' : 'SHOW'}
-            </span>
+      </section>
+
+      <section className={styles['login-section']}>
+        <h1>Login</h1>
+        <form className={styles['login-form']}>
+          <div className={styles['input-element']}>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" />
           </div>
-        </div>
-        <div className={styles['buttons-wrapper']}>
-          <button type="submit" className={styles['login-button']}>Login</button>
-          <button type="button" className={styles['sign-up-button']}>Sign Up</button>
-        </div>
-      </form>
-    </section>
+          <div className={styles['input-element']}>
+            <label htmlFor="password">Password</label>
+            <div className={styles['password-wrapper']}>
+              <input
+                type={showPassword ? 'text' : 'password'}
+                id="password"
+                name="password"
+              />
+              <span className={styles["toggle-password"]} onClick={togglePasswordVisibility}>
+                {showPassword ? 'HIDE' : 'SHOW'}
+              </span>
+            </div>
+          </div>
+          <div className={styles['buttons-wrapper']}>
+            <button type="submit" className={styles['login-button']}>Login</button>
+            <button type="button" className={styles['sign-up-button']}>Sign Up</button>
+          </div>
+        </form>
+      </section>
+    </>
   );
 }
--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 
+import Image from 'next/image';
 import React, { useState } from 'react';
 import styles from '../../styles/signup.module.css';
+import maleMascot from '../../../public/images/sign-up-avatar.png'
 
 export default function SignUp() {
   const [showPassword, setShowPassword] = useState(false);
@@ -20,61 +22,77 @@ export default function SignUp() {
   };
 
   return (
-    <section className={styles['signup-section']}>
-      <h1>Sign Up</h1>
-      <form className={styles['signup-form']}>
-        <div className={styles['input-element']}>
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" name="username" />
+
+    <>
+      <section>
+        <div className={styles['mascot-container']}>
+          <Image
+            src={maleMascot}
+            alt="Male Manguro Mascot"
+            width={500}
+            height={500}
+            className={styles['login-avatar']}
+          />
         </div>
-        <div className={styles['input-element']}>
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" />
-        </div>
-        <div className={styles['input-element']}>
-          <label htmlFor="password">Password</label>
-          <div className={styles['password-wrapper']}>
-            <input
-              type={showPassword ? 'text' : 'password'}
-              id="password"
-              name="password"
-            />
-            <span className={styles['toggle-password']} onClick={togglePasswordVisibility}>
-              {showPassword ? 'HIDE' : 'SHOW'}
-            </span>
+      </section>
+
+      <section className={styles['signup-section']}>
+        <h1>Sign Up</h1>
+        <form className={styles['signup-form']}>
+          <div className={styles['input-element']}>
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" name="username" />
           </div>
-        </div>
-        <div className={styles['input-element']}>
-          <label htmlFor="confirmPassword">Confirm Password</label>
-          <div className={styles['password-wrapper']}>
-            <input
-              type={showConfirmPassword ? 'text' : 'password'}
-              id="confirmPassword"
-              name="confirmPassword"
-            />
-            <span className={styles['toggle-password']} onClick={toggleConfirmPasswordVisibility}>
-              {showConfirmPassword ? 'HIDE' : 'SHOW'}
-            </span>
+          <div className={styles['input-element']}>
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" />
           </div>
-        </div>
-        <p className={styles['password-req']}>Password Requirement</p>
-        <ul className={styles['password-req-list']}>
-          <li>Minimum length of 8 characters</li>
-        </ul>
-        <a href="#" onClick={openPasswordGuidelines} className={styles['guidelines-link']}>
-          Recommended Password Guidelines
-        </a>
-        <div className={styles['terms-wrapper']}>
-          <input type="checkbox" id="terms" name="terms" />
-          <label htmlFor="terms">
-            I agree to the <a href="/terms">terms of service</a>
-          </label>
-        </div>
-        <div className={styles['buttons-wrapper']}>
-          <button type="submit" className={styles['sign-up-button']}>Sign Up</button>
-          <button type="button" className={styles['login-button']}>Login</button>
-        </div>
-      </form>
-    </section>
+          <div className={styles['input-element']}>
+            <label htmlFor="password">Password</label>
+            <div className={styles['password-wrapper']}>
+              <input
+                type={showPassword ? 'text' : 'password'}
+                id="password"
+                name="password"
+              />
+              <span className={styles['toggle-password']} onClick={togglePasswordVisibility}>
+                {showPassword ? 'HIDE' : 'SHOW'}
+              </span>
+            </div>
+          </div>
+          <div className={styles['input-element']}>
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <div className={styles['password-wrapper']}>
+              <input
+                type={showConfirmPassword ? 'text' : 'password'}
+                id="confirmPassword"
+                name="confirmPassword"
+              />
+              <span className={styles['toggle-password']} onClick={toggleConfirmPasswordVisibility}>
+                {showConfirmPassword ? 'HIDE' : 'SHOW'}
+              </span>
+            </div>
+          </div>
+          <p className={styles['password-req']}>Password Requirement</p>
+          <ul className={styles['password-req-list']}>
+            <li>Minimum length of 8 characters</li>
+          </ul>
+          <a href="#" onClick={openPasswordGuidelines} className={styles['guidelines-link']}>
+            Recommended Password Guidelines
+          </a>
+          <div className={styles['terms-wrapper']}>
+            <input type="checkbox" id="terms" name="terms" />
+            <label htmlFor="terms">
+              I agree to the <a href="/terms">terms of service</a>
+            </label>
+          </div>
+          <div className={styles['buttons-wrapper']}>
+            <button type="submit" className={styles['sign-up-button']}>Sign Up</button>
+            <button type="button" className={styles['login-button']}>Login</button>
+          </div>
+        </form>
+      </section>
+
+    </>
   );
 }
