a4cfdebd30a65bf7e87ba7b875295eaed33f1e51
Tathluach Chol <tathluach.krc@gmail.com>
2024-09-26 10:30:16 -0400

    Search Page Fix & Started Watchlists
    Fixed the search bar on the search page. Started adding html and styling to watchlists page.

--- frontend/src/app/user-watchlists/page.tsx
+++ frontend/src/app/user-watchlists/page.tsx
@@ -1,13 +1,32 @@
 'use client';
 
 import styles from '../../styles/watchlists.module.css';
+import SearchIcon from '../../../public/icons/search-icon.svg';
 
 export default function UserWatchlists() {
   return (
     <>
       <section>
         <h1>User's Watchlists</h1>
-        <p>Content for section</p>
+        <form className={styles.searchForm} onSubmit={(e) => e.preventDefault()}>
+            <div className={styles.searchInputWrapper}>
+              <SearchIcon className={styles.searchIcon} aria-hidden="true" />
+              <input
+                type="text"
+                placeholder="Search"
+                className={styles.searchInput}
+                aria-label="Search"
+              />
+            </div>
+          </form>
+      </section>
+
+      <section className={styles.otherSection} aria-labelledby="your-watchlists-heading">
+        <h2 id="your-watchlists-heading" className="section-rectangle">Your Watchlists</h2>
+      </section>
+
+      <section className={styles.otherSection} aria-labelledby="shared-watchlists-heading">
+        <h2 id="shared-watchlists-heading" className="section-rectangle">Your Watchlists</h2>
       </section>
     </>
   );
